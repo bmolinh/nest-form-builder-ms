2462761e3f86dfc5f6413a560c4e7b7104b7afcf
Byron Elías Molina Hermosilla <bmolina2@externos.bancoestado.cl>
2024-11-07 18:57:31 -0300

    feat: added db integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/type-orm-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TypeOrmConfigService],
 })
 export class AppModule {}
---
+++ src/database/type-orm-config.service.spec.ts
@@ -0,0 +1,51 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmConfigService } from './type-orm-config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+describe('TypeOrmConfigService', () => {
+  let service: TypeOrmConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
+      providers: [
+        TypeOrmConfigService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              const config = {
+                'database.host': 'localhost',
+                'database.port': 3306,
+                'database.user': 'testuser',
+                'database.password': 'testpassword',
+                'database.name': 'testdb',
+              };
+              return config[key];
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<TypeOrmConfigService>(TypeOrmConfigService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should create TypeOrm options', () => {
+    const options = service.createTypeOrmOptions();
+    expect(options).toEqual({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testpassword',
+      database: 'testdb',
+      entities: [],
+      synchronize: true,
+    });
+  });
+});
