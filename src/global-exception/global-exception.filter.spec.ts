a21b56152db8cd5d8bed9de758bca0fc4915f1f7
Byron Elías Molina Hermosilla <bmolina2@externos.bancoestado.cl>
2024-11-10 21:58:56 -0300

    feat: added errors handling

---
+++ src/global-exception/global-exception.filter.spec.ts
@@ -0,0 +1,65 @@
+import { GlobalExceptionFilter } from './global-exception.filter';
+import { ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+describe('GlobalExceptionFilter', () => {
+  it('should be defined', () => {
+    expect(new GlobalExceptionFilter()).toBeDefined();
+  });
+
+  describe('GlobalExceptionFilter', () => {
+    let globalExceptionFilter: GlobalExceptionFilter;
+    let mockArgumentsHost: ArgumentsHost;
+    let mockResponse: Response;
+    let mockRequest: Request;
+
+    beforeEach(() => {
+      globalExceptionFilter = new GlobalExceptionFilter();
+      mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as any;
+      mockRequest = {
+        url: '/test-url',
+      } as any;
+      mockArgumentsHost = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getResponse: () => mockResponse,
+          getRequest: () => mockRequest,
+        }),
+      } as any;
+    });
+
+    it('should be defined', () => {
+      expect(globalExceptionFilter).toBeDefined();
+    });
+
+    it('should handle HttpException correctly', () => {
+      const exception = new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+
+      globalExceptionFilter.catch(exception, mockArgumentsHost);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(HttpStatus.FORBIDDEN);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        statusCode: HttpStatus.FORBIDDEN,
+        timestamp: expect.any(String),
+        path: '/test-url',
+      });
+    });
+
+    it('should handle non-HttpException correctly', () => {
+      const exception = new Error('Some error');
+
+      globalExceptionFilter.catch(exception, mockArgumentsHost);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        timestamp: expect.any(String),
+        path: '/test-url',
+      });
+    });
+  });
+});
---
+++ src/global-exception/global-exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const httpContext = host.switchToHttp();
+    const response = httpContext.getResponse<Response>();
+    const request = httpContext.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
